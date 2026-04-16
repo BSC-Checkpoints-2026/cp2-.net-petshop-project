b0c37f1aeca47a05ab5646757bbc3e235f3479ae
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 00:21:39 -0300

    feat(ApplicationDbContext): feito a configuração do EF Core

--- PetShop.Infrastructure/Context/ApplicationDbContext.cs
+++ PetShop.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Domain.Entities;
 
 namespace PetShop.Infrastructure.Context;
 
 public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :  base(options) {}
     
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
