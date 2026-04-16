09148219113c289c5bef8019f29ce9d64f73822f
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 00:32:59 -0300

    feat(CustomerMapping): criado a base do CustomerMapping

---
+++ PetShop.Infrastructure/Mappings/CustomerMapping.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Infrastructure.Mappings;
+
+public class CustomerMapping : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        
+    }
+}
