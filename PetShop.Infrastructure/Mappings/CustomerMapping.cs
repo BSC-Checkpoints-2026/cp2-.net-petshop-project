78d21c173a58a182b9d5eb6083c3af853635e537
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 00:56:54 -0300

    feat(CustomerMapping): criado regra do Customer

--- PetShop.Infrastructure/Mappings/CustomerMapping.cs
+++ PetShop.Infrastructure/Mappings/CustomerMapping.cs
@@ -8,6 +8,43 @@ public class CustomerMapping : IEntityTypeConfiguration<Customer>
 {
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
+        builder.ToTable("Customers");
         
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.FirstName)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(c => c.LastName)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(c => c.Cpf)
+            .IsRequired()
+            .HasMaxLength(11);
+
+        builder.Property(c => c.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+        
+        builder.Property(c => c.PhoneNumber)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        builder.Property(c => c.Address)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.HasIndex(c => c.Cpf)
+            .IsUnique();
+
+        builder.HasMany(c => c.Pets)
+            .WithOne(p => p.Customer)
+            .HasForeignKey(p => p.CustomerId);
+
+        builder.HasMany(c => c.Orders)
+            .WithOne(o => o.Customer)
+            .HasForeignKey(o => o.CustomerId);
     }
 }
