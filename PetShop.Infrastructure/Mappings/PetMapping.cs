b24e1cc96a3901fad3d7284f479552394edb52a4
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 01:28:34 -0300

    feat(PetMapping): criado regras do PetMapping

--- PetShop.Infrastructure/Mappings/PetMapping.cs
+++ PetShop.Infrastructure/Mappings/PetMapping.cs
@@ -1,6 +1,47 @@
-﻿namespace PetShop.Infrastructure.Mappings;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShop.Domain.Entities;
 
-public class PetMapping
+namespace PetShop.Infrastructure.Mappings;
+
+public class PetMapping : IEntityTypeConfiguration<Pet>
 {
-    
+    public void Configure(EntityTypeBuilder<Pet> builder)
+    {
+        builder.ToTable("Pets");
+        
+        builder.HasKey(p => p.Id);
+        
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(p => p.Species)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(p => p.Breed)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(p => p.BirthDate)
+            .IsRequired();
+
+        builder.Property(p => p.Weight)
+            .IsRequired()
+            .HasPrecision(5, 2);
+
+        builder.Property(p => p.Gender)
+            .IsRequired();
+
+        builder.HasOne(p => p.Customer)
+            .WithMany(c => c.Pets)
+            .HasForeignKey(p => p.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(p => p.Appointments)
+            .WithOne(a => a.Pet)
+            .HasForeignKey(a => a.PetId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
