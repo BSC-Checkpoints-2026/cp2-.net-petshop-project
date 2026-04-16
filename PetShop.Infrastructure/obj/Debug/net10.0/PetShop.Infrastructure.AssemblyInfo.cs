12e149cb2f815c135560c1db8e7df141b6ccc523
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 01:32:53 -0300

    chore: dependências do infrastructure

--- PetShop.Infrastructure/obj/Debug/net10.0/PetShop.Infrastructure.AssemblyInfo.cs
+++ PetShop.Infrastructure/obj/Debug/net10.0/PetShop.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4f3162b619907d0a53d9453a7496f91c06ba70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d21c173a58a182b9d5eb6083c3af853635e537")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
