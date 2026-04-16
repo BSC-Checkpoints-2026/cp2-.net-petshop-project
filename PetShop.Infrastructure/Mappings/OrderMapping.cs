da185b297cfea575f3cb45e06210c50f9eb5eecd
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 20:39:54 -0300

    feat(OrderMapping): criado as regras do OrderMapping

--- PetShop.Infrastructure/Mappings/OrderMapping.cs
+++ PetShop.Infrastructure/Mappings/OrderMapping.cs
@@ -1,6 +1,32 @@
-﻿namespace PetShop.Infrastructure.Mappings;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShop.Domain.Entities;
 
-public class OrderMapping
+namespace PetShop.Infrastructure.Mappings;
+
+public class OrderMapping : IEntityTypeConfiguration<Order>
 {
-    
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.OrderDate)
+            .IsRequired();
+
+        builder.Property(o => o.TotalAmount)
+            .IsRequired()
+            .HasPrecision(7, 2);
+
+        builder.HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(o => o.OrderItems)
+            .WithOne(i => i.Order)
+            .HasForeignKey(i => i.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
