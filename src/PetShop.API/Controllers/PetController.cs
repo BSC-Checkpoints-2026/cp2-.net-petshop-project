d552dbe7824cf8330b02425100e33537afbb2618
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 20:27:37 -0300

    feat(PetController): criado endpoint PetController

---
+++ src/PetShop.API/Controllers/PetController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Application.DTOs.Pet;
+using PetShop.Application.Interfaces.Pet;
+
+namespace PetShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PetController : ControllerBase
+{
+    private readonly IPetService _petService;
+
+    public PetController(IPetService petService)
+    {
+        _petService = petService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var pets = await _petService.GetAllAsync();
+        return Ok(pets);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var pet = await _petService.GetByIdAsync(id);
+
+        if (pet is null)
+            return NotFound();
+
+        return Ok(pet);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreatePetDto dto)
+    {
+        var createdPet = await _petService.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = createdPet.Id }, createdPet);
+    }
+}
