ce5c10d5680839d20d410346215dbbcb63980add
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 19:27:59 -0300

    feat(ProductController): feito a criação do ProductController

---
+++ src/PetShop.API/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Application.DTOs.Product;
+using PetShop.Application.Interfaces.Product;
+
+namespace PetShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+
+        if (product is null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateProductDto dto)
+    {
+        var createdProduct = await _productService.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = createdProduct.Id }, createdProduct);
+    }
+}
