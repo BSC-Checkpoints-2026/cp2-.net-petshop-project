18e348dcce7c33bf8bce7e3e708aca426a14db61
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 17:17:11 -0300

    refactor(CustomerController): feito troca do repository para service

--- src/PetShop.API/Controllers/CustomerController.cs
+++ src/PetShop.API/Controllers/CustomerController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Application.DTOs.Customer;
 using PetShop.Application.Interfaces.Customer;
-using PetShop.Domain.Entities;
 
 namespace PetShop.Controllers;
 
@@ -8,24 +8,24 @@ namespace PetShop.Controllers;
 [Route("api/[controller]")]
 public class CustomerController : ControllerBase
 {
-    private readonly ICustomerRepository _customerRepository;
+    private readonly ICustomerService _customerService;
 
-    public CustomerController(ICustomerRepository customerRepository)
+    public CustomerController(ICustomerService customerService)
     {
-        _customerRepository = customerRepository;
+        _customerService = customerService;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var customers = await _customerRepository.GetAllAsync();
+        var customers = await _customerService.GetAllAsync();
         return Ok(customers);
     }
 
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById(Guid id)
     {
-        var customer = await _customerRepository.GetByIdAsync(id);
+        var customer = await _customerService.GetByIdAsync(id);
 
         if (customer is null)
             return NotFound();
@@ -34,9 +34,10 @@ public class CustomerController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] Customer customer)
+    public async Task<IActionResult> Create([FromBody] CreateCustomerDto dto)
     {
-        await _customerRepository.AddAsync(customer);
-        return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        var createdCustomer = await _customerService.CreateAsync(dto);
+
+        return CreatedAtAction(nameof(GetById), new { id = createdCustomer.Id }, createdCustomer);
     }
 }
