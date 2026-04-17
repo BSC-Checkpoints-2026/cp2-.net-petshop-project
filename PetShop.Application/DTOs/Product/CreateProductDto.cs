bde85a910a1e2d19af34c3127c0e4d1bc1fb90e0
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 18:41:19 -0300

    feat(CreateProductDto): feito a criação do request CreateProductDto

---
+++ PetShop.Application/DTOs/Product/CreateProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PetShop.Application.DTOs.Product;
+
+public class CreateProductDto
+{
+    public string Name { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+    public decimal Price { get; set; }
+}
