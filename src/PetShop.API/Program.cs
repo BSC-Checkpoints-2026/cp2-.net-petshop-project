24162ed9718f987780b00080d02886d8ef247bfb
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 23:38:24 -0300

    feat(PetShop.API): criado reference para PetShop.Infrastructure

--- src/PetShop.API/Program.cs
+++ src/PetShop.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PetShop.Infrastructure.Context;
+
 namespace petShop;
 
 public class Program
@@ -7,24 +10,21 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite("Data Source=petshop.db"));
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-
         app.MapControllers();
 
         app.Run();
