ba54a9506c845698b3428bdc56495cc41f7edec6
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-16 23:39:42 -0300

    chore: feito instalação do Microsoft.EntityFrameworkCore.Sqlite

--- src/PetShop.API/obj/Debug/net10.0/PetShop.API.AssemblyInfo.cs
+++ src/PetShop.API/obj/Debug/net10.0/PetShop.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8f00a096fda7224b8b65e999e16ae17dd19ef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24162ed9718f987780b00080d02886d8ef247bfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
