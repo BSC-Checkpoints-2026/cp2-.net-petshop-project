8f7416802bc89a323b36ac15a000dc644194f47c
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 19:03:42 -0300

    feat(IProductRepository): feito interface IProductRepository

---
+++ PetShop.Application/Interfaces/Product/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PetShop.Application.Interfaces.Product;
+
+using PetShop.Domain.Entities;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(Guid id);
+    Task AddAsync(Product product);
+}
