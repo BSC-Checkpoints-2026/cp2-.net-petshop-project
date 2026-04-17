72751441b4e00089a11492c783b1caf62a5be7b3
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 20:21:53 -0300

    feat(PetRepository): criado o PetRepository

---
+++ PetShop.Infrastructure/Repositories/PetRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Application.Interfaces.Pet;
+using PetShop.Domain.Entities;
+using PetShop.Infrastructure.Context;
+
+namespace PetShop.Infrastructure.Repositories;
+
+public class PetRepository : IPetRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public PetRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Pet>> GetAllAsync()
+    {
+        return await _context.Pets.ToListAsync();
+    }
+
+    public async Task<Pet?> GetByIdAsync(Guid id)
+    {
+        return await _context.Pets.FindAsync(id);
+    }
+
+    public async Task AddAsync(Pet pet)
+    {
+        await _context.Pets.AddAsync(pet);
+        await _context.SaveChangesAsync();
+    }
+}
