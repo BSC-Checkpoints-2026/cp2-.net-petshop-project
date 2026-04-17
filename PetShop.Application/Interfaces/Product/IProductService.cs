990bdb047c68f5d247c868ced731569dbbc490db
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 19:06:36 -0300

    feat(IProductService): criado a interface IProductService

---
+++ PetShop.Application/Interfaces/Product/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using PetShop.Application.DTOs.Product;
+
+namespace PetShop.Application.Interfaces.Product;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductResponseDto>> GetAllAsync();
+    Task<ProductResponseDto?> GetByIdAsync(Guid id);
+    Task<ProductResponseDto> CreateAsync(CreateProductDto dto);
+}
