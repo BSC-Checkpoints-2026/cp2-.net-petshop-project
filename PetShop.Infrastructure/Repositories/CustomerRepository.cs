97f201506b0e7531da626aaf99b1dffbfd146e36
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 13:56:46 -0300

    feat(CustomerRepository): feito crud do CustomerRepository

--- PetShop.Infrastructure/Repositories/CustomerRepository.cs
+++ PetShop.Infrastructure/Repositories/CustomerRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace PetShop.Infrastructure.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Application.Interfaces;
+using PetShop.Domain.Entities;
+using PetShop.Infrastructure.Context;
 
-public class CustomerRepository
+namespace PetShop.Infrastructure.Repositories;
+
+public class CustomerRepository : ICustomerRepository
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public CustomerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+
+    public async Task<Customer?> GetByIdAsync(Guid id)
+    {
+        return await _context.Customers.FindAsync(id);
+    }
+
+    public async Task AddAsync(Customer customer)
+    {
+        await _context.Customers.AddAsync(customer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Customer customer)
+    {
+        _context.Customers.Update(customer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Customer customer)
+    {
+        _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+    }
 }
