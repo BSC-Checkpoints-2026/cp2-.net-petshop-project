1c3ef837a01cfe65dafe93723834bb9d317d28fe
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 17:06:24 -0300

    feat(CustomerService): feito a criação do CustomerService

---
+++ PetShop.Application/Services/CustomerService.cs
@@ -0,0 +1,75 @@
+﻿using PetShop.Application.DTOs.Customer;
+using PetShop.Application.Interfaces.Customer;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Application.Services;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public CustomerService(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task<IEnumerable<CustomerResponseDto>> GetAllAsync()
+    {
+        var customers = await _customerRepository.GetAllAsync();
+
+        return customers.Select(c => new CustomerResponseDto
+        {
+            Id = c.Id,
+            FirstName = c.FirstName,
+            LastName = c.LastName,
+            Cpf = c.Cpf,
+            Email = c.Email,
+            PhoneNumber = c.PhoneNumber,
+            Address = c.Address
+        });
+    }
+
+    public async Task<CustomerResponseDto?> GetByIdAsync(Guid id)
+    {
+        var customer = await _customerRepository.GetByIdAsync(id);
+
+        if (customer is null)
+            return null;
+
+        return new CustomerResponseDto
+        {
+            Id = customer.Id,
+            FirstName = customer.FirstName,
+            LastName = customer.LastName,
+            Cpf = customer.Cpf,
+            Email = customer.Email,
+            PhoneNumber = customer.PhoneNumber,
+            Address = customer.Address
+        };
+    }
+
+    public async Task<CustomerResponseDto> CreateAsync(CreateCustomerDto dto)
+    {
+        var customer = new Customer(
+            dto.FirstName,
+            dto.LastName,
+            dto.Cpf,
+            dto.Email,
+            dto.PhoneNumber,
+            dto.Address
+        );
+
+        await _customerRepository.AddAsync(customer);
+
+        return new CustomerResponseDto
+        {
+            Id = customer.Id,
+            FirstName = customer.FirstName,
+            LastName = customer.LastName,
+            Cpf = customer.Cpf,
+            Email = customer.Email,
+            PhoneNumber = customer.PhoneNumber,
+            Address = customer.Address
+        };
+    }
+}
