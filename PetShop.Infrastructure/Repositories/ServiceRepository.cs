9845825fd66474287c3f5e117bb90e5127c7ba28
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 01:07:46 -0300

    feat(ServiceRepository): criado o ServiceRepository

---
+++ PetShop.Infrastructure/Repositories/ServiceRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Application.Interfaces.Service;
+using PetShop.Domain.Entities;
+using PetShop.Infrastructure.Context;
+
+namespace PetShop.Infrastructure.Repositories;
+
+public class ServiceRepository : IServiceRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ServiceRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Service>> GetAllAsync()
+    {
+        return await _context.Services.ToListAsync();
+    }
+
+    public async Task<Service?> GetByIdAsync(Guid id)
+    {
+        return await _context.Services.FindAsync(id);
+    }
+
+    public async Task AddAsync(Service service)
+    {
+        await _context.Services.AddAsync(service);
+        await _context.SaveChangesAsync();
+    }
+}
