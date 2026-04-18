fe816013846a89239f0510e4ef3ecfa694bde749
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:04:05 -0300

    feat(OrderResponseDto): criado response OrderResponseDto

--- PetShop.Application/DTOs/Order/OrderResponseDto.cs
+++ PetShop.Application/DTOs/Order/OrderResponseDto.cs
@@ -2,5 +2,8 @@
 
 public class OrderResponseDto
 {
-    
+    public Guid Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public Guid CustomerId { get; set; }
 }
