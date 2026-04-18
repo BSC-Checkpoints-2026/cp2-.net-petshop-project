7ff0fc1cbac2c084e059996410a7a9faabbf703e
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:45:42 -0300

    feat(OrderItemRepository): criado o OrderItemRepository

---
+++ PetShop.Infrastructure/Repositories/OrderItemRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Application.Interfaces.OrderItem;
+using PetShop.Domain.Entities;
+using PetShop.Infrastructure.Context;
+
+namespace PetShop.Infrastructure.Repositories;
+
+public class OrderItemRepository : IOrderItemRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderItemRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<OrderItem>> GetAllAsync()
+    {
+        return await _context.OrderItems.ToListAsync();
+    }
+
+    public async Task<OrderItem?> GetByIdAsync(Guid id)
+    {
+        return await _context.OrderItems.FindAsync(id);
+    }
+
+    public async Task AddAsync(OrderItem orderItem)
+    {
+        await _context.OrderItems.AddAsync(orderItem);
+        await _context.SaveChangesAsync();
+    }
+}
