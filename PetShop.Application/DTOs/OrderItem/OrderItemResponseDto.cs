3c338c075e6e6e57c3f4cf0ba753c417bc60701d
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:38:02 -0300

    feat(OrderItemResponseDto): criado response OrderItemResponseDto

--- PetShop.Application/DTOs/OrderItem/OrderItemResponseDto.cs
+++ PetShop.Application/DTOs/OrderItem/OrderItemResponseDto.cs
@@ -2,5 +2,10 @@
 
 public class OrderItemResponseDto
 {
-    
+    public Guid Id { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
 }
