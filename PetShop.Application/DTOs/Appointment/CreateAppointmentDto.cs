6c18121ede648601e75560744e27a9a0e37b2ba9
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 23:09:20 -0300

    feat(CreateAppointmentDto): criado request CreateAppointmentDto

---
+++ PetShop.Application/DTOs/Appointment/CreateAppointmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PetShop.Application.DTOs.Appointment;
+
+public class CreateAppointmentDto
+{
+    public DateTime AppointmentDate { get; set; }
+    public Guid PetId { get; set; }
+    public Guid ServiceId { get; set; }
+    public Guid EmployeeId { get; set; }
+}
