6a825f6c87f50bbe6030886f4a0c85442a452b42
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:52:33 -0300

    feat(OrderItemController): criado endpoint OrderItemController

---
+++ src/PetShop.API/Controllers/OrderItemController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Application.DTOs.OrderItem;
+using PetShop.Application.Interfaces.OrderItem;
+
+namespace PetShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderItemController : ControllerBase
+{
+    private readonly IOrderItemService _service;
+
+    public OrderItemController(IOrderItemService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _service.GetAllAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateOrderItemDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+        return Created("", result);
+    }
+}
