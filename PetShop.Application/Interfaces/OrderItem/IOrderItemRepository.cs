573d0fdd5e72979d7bb1357ba4b65ada2da0f1a6
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:39:56 -0300

    feat(IOrderItemRepository): criado interface IOrderItemRepository

---
+++ PetShop.Application/Interfaces/OrderItem/IOrderItemRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PetShop.Application.Interfaces.OrderItem;
+
+using PetShop.Domain.Entities;
+
+public interface IOrderItemRepository
+{
+    Task<IEnumerable<OrderItem>> GetAllAsync();
+    Task<OrderItem?> GetByIdAsync(Guid id);
+    Task AddAsync(OrderItem orderItem);
+}
