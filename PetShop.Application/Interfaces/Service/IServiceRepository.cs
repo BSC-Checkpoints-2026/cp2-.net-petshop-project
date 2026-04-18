d9283b31ee472be193a3c24cca477f128d161a14
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 01:04:13 -0300

    feat(IServiceRepository): criado interface IServiceRepository

---
+++ PetShop.Application/Interfaces/Service/IServiceRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PetShop.Application.Interfaces.Service;
+
+using PetShop.Domain.Entities;
+
+public interface IServiceRepository
+{
+    Task<IEnumerable<Service>> GetAllAsync();
+    Task<Service?> GetByIdAsync(Guid id);
+    Task AddAsync(Service service);
+}
---
+++ PetShop.Application/Interfaces/Service/IServiceService.cs
@@ -0,0 +1,6 @@
+﻿namespace PetShop.Application.Interfaces.Service;
+
+public interface IServiceService
+{
+    
+}
