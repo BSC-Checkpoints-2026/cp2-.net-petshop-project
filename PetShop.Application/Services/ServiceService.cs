2b933781613c3f238c5a37818cbb05fdb643186d
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 01:09:11 -0300

    feat(ServiceService): criadp as regras do ServiceService

--- PetShop.Application/Services/ServiceService.cs
+++ PetShop.Application/Services/ServiceService.cs
@@ -1,6 +1,67 @@
-﻿namespace PetShop.Application.Services;
+﻿using PetShop.Application.DTOs.Service;
+using PetShop.Application.Interfaces.Service;
+using PetShop.Domain.Entities;
 
-public class ServiceService
+namespace PetShop.Application.Services;
+
+public class ServiceService : IServiceService
 {
-    
+    private readonly IServiceRepository _serviceRepository;
+
+    public ServiceService(IServiceRepository serviceRepository)
+    {
+        _serviceRepository = serviceRepository;
+    }
+
+    public async Task<IEnumerable<ServiceResponseDto>> GetAllAsync()
+    {
+        var services = await _serviceRepository.GetAllAsync();
+
+        return services.Select(s => new ServiceResponseDto
+        {
+            Id = s.Id,
+            Name = s.Name,
+            Description = s.Description,
+            Price = s.Price,
+            DurationInMinutes = s.DurationInMinutes
+        });
+    }
+
+    public async Task<ServiceResponseDto?> GetByIdAsync(Guid id)
+    {
+        var service = await _serviceRepository.GetByIdAsync(id);
+
+        if (service is null)
+            return null;
+
+        return new ServiceResponseDto
+        {
+            Id = service.Id,
+            Name = service.Name,
+            Description = service.Description,
+            Price = service.Price,
+            DurationInMinutes = service.DurationInMinutes
+        };
+    }
+
+    public async Task<ServiceResponseDto> CreateAsync(CreateServiceDto dto)
+    {
+        var service = new Service(
+            dto.Name,
+            dto.Description,
+            dto.Price,
+            dto.DurationInMinutes
+        );
+
+        await _serviceRepository.AddAsync(service);
+
+        return new ServiceResponseDto
+        {
+            Id = service.Id,
+            Name = service.Name,
+            Description = service.Description,
+            Price = service.Price,
+            DurationInMinutes = service.DurationInMinutes
+        };
+    }
 }
