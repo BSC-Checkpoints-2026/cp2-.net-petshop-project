8eb899a601de3ff1061b7b9471b9eda849c4bd66
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:24:59 -0300

    feat(OrderController): criado endpoint OrderController

---
+++ src/PetShop.API/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Application.DTOs.Order;
+using PetShop.Application.Interfaces.Order;
+
+namespace PetShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var orders = await _orderService.GetAllAsync();
+        return Ok(orders);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var order = await _orderService.GetByIdAsync(id);
+
+        if (order is null)
+            return NotFound();
+
+        return Ok(order);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateOrderDto dto)
+    {
+        var createdOrder = await _orderService.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = createdOrder.Id }, createdOrder);
+    }
+}
