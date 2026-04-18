9401813da4c7eac7ab39f667a58cd423e79d3e27
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-17 23:42:25 -0300

    feat: adicionado propriedade status

--- PetShop.Application/DTOs/Appointment/AppointmentResponseDto.cs
+++ PetShop.Application/DTOs/Appointment/AppointmentResponseDto.cs
@@ -4,6 +4,7 @@ public class AppointmentResponseDto
 {
     public Guid Id { get; set; }
     public DateTime AppointmentDate { get; set; }
+    public int Status { get; set; }
     public Guid PetId { get; set; }
     public Guid ServiceId { get; set; }
     public Guid EmployeeId { get; set; }
--- PetShop.Application/DTOs/Appointment/CreateAppointmentDto.cs
+++ PetShop.Application/DTOs/Appointment/CreateAppointmentDto.cs
@@ -3,6 +3,7 @@
 public class CreateAppointmentDto
 {
     public DateTime AppointmentDate { get; set; }
+    public int Status { get; set; }
     public Guid PetId { get; set; }
     public Guid ServiceId { get; set; }
     public Guid EmployeeId { get; set; }
