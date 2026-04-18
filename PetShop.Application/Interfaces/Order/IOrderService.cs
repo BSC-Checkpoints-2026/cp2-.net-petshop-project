df37c8f5d307894b13b24421d7dd4c8816ab4f3c
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 00:07:42 -0300

    feat(IOrderService): criado interface IOrderService

---
+++ PetShop.Application/Interfaces/Order/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using PetShop.Application.DTOs.Order;
+
+namespace PetShop.Application.Interfaces.Order;
+
+public interface IOrderService
+{
+    Task<IEnumerable<OrderResponseDto>> GetAllAsync();
+    Task<OrderResponseDto?> GetByIdAsync(Guid id);
+    Task<OrderResponseDto> CreateAsync(CreateOrderDto dto);
+}
