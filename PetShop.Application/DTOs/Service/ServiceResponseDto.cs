bac217ff91b5d1d37db7886082595913b1eee91c
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-18 01:02:33 -0300

    feat(ServiceResponseDto): criado response ServiceResponseDto

--- PetShop.Application/DTOs/Service/ServiceResponseDto.cs
+++ PetShop.Application/DTOs/Service/ServiceResponseDto.cs
@@ -2,5 +2,9 @@
 
 public class ServiceResponseDto
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int DurationInMinutes { get; set; }
 }
