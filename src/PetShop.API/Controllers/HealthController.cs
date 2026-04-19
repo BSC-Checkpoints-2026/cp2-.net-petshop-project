90b1648af5ee03511d8e59bb05607f08a1d3f633
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-19 11:43:29 -0300

    feat(HealthController): criado endpoint Health

---
+++ src/PetShop.API/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "OK",
+            message = "API está funcionando"
+        });
+    }
+}
